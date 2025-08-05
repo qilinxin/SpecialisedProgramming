7876b24c123b621d7f9a8337c0df6bd6a1cbb106
qilinxin <qilinxin21@gmail.com>
2025-08-05 13:08:01 +0930

    submit to test scope

---
+++ block1/Inchworm.java
@@ -0,0 +1,32 @@
+public class Inchworm {
+
+  public int lunchtime(int branch, int rest, int leaf) {
+    //check parameters if needed
+
+    int eatCount = 0;
+    int currentStep = 0;
+
+    while (currentStep <= branch) {
+
+      if (currentStep % leaf == 0) {
+        eatCount++;
+        System.out.println("currentStep =====" + currentStep);
+      }
+      currentStep += rest;
+    }
+
+    return eatCount;
+  }
+
+
+  public static void main(String[] args) {
+    Inchworm inchworm = new Inchworm();
+//    int res = inchworm.lunchtime(11, 2,4);
+//    int res = inchworm.lunchtime(12, 6,4);
+//    int res = inchworm.lunchtime(20, 3,7);
+//    int res = inchworm.lunchtime(15, 16,5);
+//    int res = inchworm.lunchtime(1000, 3,7);
+    int res = inchworm.lunchtime(1000, 7,3);
+    System.out.println("res =====" + res);
+  }
+}
