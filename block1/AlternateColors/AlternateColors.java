96ae00c89fcab864f681f057f47cf4eddb4a7738
qilinxin <qilinxin21@gmail.com>
2025-08-12 13:20:18 +0930

    commet main function and add some verify

--- block1/AlternateColors/AlternateColors.java
+++ block1/AlternateColors/AlternateColors.java
@@ -1,13 +1,15 @@
 public class AlternateColors {
 
   static String getColor(long r, long g, long b, long k) {
+    long total = r + g + b;
+    if (k < 1 || k > total) throw new IllegalArgumentException("k out of range");
 
     long wholeLoopCount = Math.min(r, Math.min(g, b));
-    if (k <= 3 * wholeLoopCount) {
+    if (k <= 3L * wholeLoopCount) {
       long module = k % 3;
       return module == 1 ? "RED" : (module == 2 ? "GREEN" : "BLUE");
     }
-    k = k - 3 * wholeLoopCount;
+    k = k - 3L * wholeLoopCount;
     r -= wholeLoopCount;
     g -= wholeLoopCount;
     b -= wholeLoopCount;
@@ -29,13 +31,13 @@ public class AlternateColors {
     return k % 2 == 0 ? "GREEN" : "RED";
   }
 
-  public static void main(String[] args) {
-    long r = 653;
-    long g = 32;
-    long b = 1230;
-    long k = 556;
-
-    String res = getColor(r, g, b, k);
-    System.out.println(res);
-  }
+//  public static void main(String[] args) {
+//    long r = 28;
+//    long g = 4;
+//    long b = 16;
+//    long k = 40;
+//
+//    String res = getColor(r, g, b, k);
+//    System.out.println(res);
+//  }
 }
