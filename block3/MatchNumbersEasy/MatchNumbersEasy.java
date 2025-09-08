1cc5bcf862185bea84b42aead78d59ea52e03267
qilinxin <qilinxin21@gmail.com>
2025-09-08 18:38:10 +0930

    fix bugs

--- block3/MatchNumbersEasy/MatchNumbersEasy.java
+++ block3/MatchNumbersEasy/MatchNumbersEasy.java
@@ -1,45 +1,42 @@
 public class MatchNumbersEasy {
-  public static String maxNumber(int[] matches, int n) {
-    final int NEG = -1000;
-    int[] f = new int[n + 1];
-    for (int i = 1; i <= n; i++) f[i] = NEG;
-    f[0] = 0;
-    for (int x = 1; x <= n; x++) {
-      int best = NEG;
-      for (int d = 0; d < matches.length; d++) {
-        int c = matches[d];
-        if (x >= c && f[x - c] != NEG) {
-          best = Math.max(best, f[x - c] + 1);
-        }
-      }
-      f[x] = best;
-    }
-    if (f[n] <= 0) return "";
-    int need = f[n];
-    int rem = n;
-    StringBuilder sb = new StringBuilder();
-    for (int pos = 0; pos < need; pos++) {
-      for (int d = matches.length - 1; d >= 0; d--) {
-        if (pos == 0 && need > 1 && d == 0) continue;
-        int c = matches[d];
-        if (rem >= c) {
-          int left = rem - c;
-          // the value of f[left] can still reach  need - pos - 1？
-          if (f[left] == need - pos - 1) {
-            sb.append((char)('0' + d));
-            rem = left;
-            break;
-          }
+  public static String maxNumber(int[] cost, int n) {
+    int m = cost.length;
+    if (m == 1) return "0";
+
+    int minAll = Integer.MAX_VALUE, digMinAll = -1;
+    for (int d = 0; d < m; d++) if (cost[d] < minAll) { minAll = cost[d]; digMinAll = d; }
+
+    int minFirst = Integer.MAX_VALUE, digMinFirst = -1;
+    for (int d = 1; d < m; d++) if (cost[d] < minFirst) { minFirst = cost[d]; digMinFirst = d; }
+
+    if (n < minFirst) return cost[0] <= n ? "0" : ""; // 至少能放单个0
+
+    int L = 1 + (n - minFirst) / minAll;
+
+    int[] digits = new int[L];
+    digits[0] = digMinFirst;
+    for (int i = 1; i < L; i++) digits[i] = digMinAll;
+    int used = minFirst + (L - 1) * minAll;
+
+    for (int i = 0; i < L; i++) {
+      for (int d = m - 1; d >= 0; d--) {
+        if (i == 0 && L > 1 && d == 0) continue;
+        int extra = cost[d] - cost[digits[i]];
+        if (extra >= 0 && used + extra <= n) {
+          digits[i] = d; used += extra; break;
         }
       }
     }
+
+    StringBuilder sb = new StringBuilder(L);
+    for (int d : digits) sb.append((char)('0' + d));
     return sb.toString();
   }
 
   public static void main(String[] args) {
-    int[] aa = {6,3,4,5,2,3};
-    int  matches = 15;;
-    String res = maxNumber(aa, matches);
-    System.out.println(res);
+    System.out.println(maxNumber(new int[]{5,23,24}, 30));              // 20
+    System.out.println(maxNumber(new int[]{4,23,24}, 30));              // 20
+    System.out.println(maxNumber(new int[]{6,2,5,5,4,5,6,3,7,6}, 5));   // 71
+    System.out.println(maxNumber(new int[]{2}, 2));                     // 0
   }
 }
