9c191ec7a8a97e4b725745ee097117163a2df427
qilinxin <qilinxin21@gmail.com>
2025-10-13 20:22:40 +1030

    cat

---
+++ block4/CatGame/CatGame.java
@@ -0,0 +1,14 @@
+public class CatGame {
+
+  public static int getNumber(int[] coordinates, int X) {
+
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    int[] coordinates = {-3, 0, 1};
+    int X = 3;
+    int res = getNumber(coordinates, X);
+    System.out.println("res ====== "+res);
+  }
+}
