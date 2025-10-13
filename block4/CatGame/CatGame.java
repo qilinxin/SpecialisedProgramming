87a848825753c47997e8bda18564edb15abcc6d7
qilinxin <qilinxin21@gmail.com>
2025-10-13 20:27:17 +1030

    fix bugs

--- block4/CatGame/CatGame.java
+++ block4/CatGame/CatGame.java
@@ -1,8 +1,29 @@
+import java.util.Arrays;
+
 public class CatGame {
 
+  // Method signature as required
   public static int getNumber(int[] coordinates, int X) {
 
-    return 0;
+    // sort
+    Arrays.sort(coordinates);
+
+    int n = coordinates.length;
+    long best = Long.MAX_VALUE;
+
+    for (int k = 0; k <= n; k++) {
+      long mn = Long.MAX_VALUE;
+      long mx = Long.MIN_VALUE;
+
+      for (int i = 0; i < n; i++) {
+        long pos = (i < k) ? (long)coordinates[i] + X : (long)coordinates[i] - X;
+        if (pos < mn) mn = pos;
+        if (pos > mx) mx = pos;
+      }
+      long span = mx - mn;
+      if (span < best) best = span;
+    }
+    return (int) best;
   }
 
   public static void main(String[] args) {
